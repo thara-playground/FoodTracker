2ff364d0defa75b65d6ea79cb81b8fef697a7d22
Tomochika Hara <tomochikahara@gmail.com>
2016-05-29 01:01:06 +0900

    Initial commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Tomochika Hara on 2016/05/24.
+//  Copyright © 2016年 Tomochika Hara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
